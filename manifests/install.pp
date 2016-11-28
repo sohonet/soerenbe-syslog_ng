612459a61b81065166ca301bad852c48b66da81f
Jason Pereira <jason.pereira@sohonet.com>
2016-11-28 16:30:10 +0000

    breaking out configuration from install class

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,38 +1,12 @@
 # Install syslog-ng with basic logging
 class syslog_ng::install {
-  include syslog_ng::params
-  package { 'syslog-ng':
-    ensure  => present,
-  }
+
   package { 'rsyslog':
     ensure  => absent,
+  }->
+
+  package { $::syslog_ng::package_name:
+    ensure  => present,
   }
-  file { '/etc/syslog-ng/':
-    ensure  => directory,
-    recurse => true,
-    purge   => true,
-    force   => true,
-  }
-  file { '/etc/syslog-ng/scl.conf':
-    source => 'puppet:///modules/syslog_ng/scl.conf',
-  }
-  file { $::syslog_ng::config_dir:
-    ensure  => directory,
-    recurse => true,
-    purge   => true,
-  }
-  # Basic configuration
-  file { '/etc/syslog-ng/syslog-ng.conf':
-    content => template('syslog_ng/syslog-ng.conf.erb'),
-    notify  => Service[syslog_ng],
-  }
-  # Basic logging
-  syslog_ng::source::system {$::syslog_ng::local_source: }
-  syslog_ng::default {'default':
-    source    => $::syslog_ng::local_source,
-    directory => $::syslog_ng::system_log_dir,
-    owner     => 'root',
-    group     => 'syslog',
-    perm      => '0775',
-  }
+
 }
